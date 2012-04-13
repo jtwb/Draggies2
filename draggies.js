be9b55fd8a2f180c17253a38b083792b79d4f21b
Jason Benterou <jason.benterou+github@gmail.com>
2012-04-13 03:44:24 -0700

    Cleanup js

--- draggies.js
+++ draggies.js
@@ -49,22 +49,5 @@ if (Meteor.is_client) {
           Draggies.update(this._id, {$set: {value: $(e.target).val() } });
       }
   };
-
-  Template.hello.greeting = function () {
-    return "Welcome to draggies.";
-  };
-
-  Template.hello.events = {
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  };
 }
 
-if (Meteor.is_server) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
-}
